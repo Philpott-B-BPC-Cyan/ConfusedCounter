c57034cea44b2b73d993ccf9ece0d290690d1200
Philpott-B-BPC-Cyan <32195086+Philpott-B-BPC-Cyan@users.noreply.github.com>
2017-11-10 09:54:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<html>
+  <body>
 
-?>
+    <?php
+
+    $x = 1;
+    while($x <= 9) {
+      echo "$x <br>";
+      $x++;
+    }
+    
+    echo "dammit I'm like a confused 6";
+
+     ?>
+   
+  </body>
+</html>
